f7ef5fdc1006ba8ec7ee821d85dd7dded1cb626f
Diana Marzjan <dianafa@o2.pl>
2021-03-02 21:13:47 +0100

    Style the welcome page: input and button

--- src/components/IngredientsListInput.tsx
+++ src/components/IngredientsListInput.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
+import {Button, TextField} from '@material-ui/core';
+import './IngredientsListInput.css';
 
 const parseIngredientsInput = (text: string): Array<string> => {
   return text.split(',').map(ingredient => ingredient.trim());
 };
 
-export const IngredientsListInput = ({onSubmit}: {onSubmit: any}) => {
+export const IngredientsListInput = ({onSubmit}: { onSubmit: any }) => {
   const [inputText, setInputText] = useState('');
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -14,14 +16,21 @@ export const IngredientsListInput = ({onSubmit}: {onSubmit: any}) => {
 
   const handleChange = (event: any) => {
     setInputText(event.target.value);
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Ingredients:
-        <input type="text" value={inputText} onChange={handleChange}/>
-      </label>
-      <input type="submit" value="Submit" />
-    </form>);
+    <div className="ingredient-list-input">
+    <form className="ingredient-list-input-form" onSubmit={handleSubmit}>
+      <TextField
+        id="outlined-multiline-static"
+        label="Ingredients"
+        multiline
+        rows={4}
+        variant="outlined"
+        value={inputText}
+        onChange={handleChange}
+      />
+      <Button id="ingredient-list-submit-button" type="submit" variant="contained" color="primary">Submit</Button>
+    </form>
+    </div>);
 };
