362736889c07c8c2fcf3a3edb2e5403d372b55ed
Diana Marzjan <dianafa@o2.pl>
2021-03-03 19:29:35 +0100

    Display a clean white result list

--- src/components/IngredientsResult.tsx
+++ src/components/IngredientsResult.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import {IngredientInfo} from '../types/IngredientInfo';
 
-export const IngredientsResult = ({results} : {results: Array<IngredientInfo>}) => {
+import {createStyles, Theme, makeStyles} from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      margin: 'auto',
+      backgroundColor: theme.palette.background.paper,
+    },
+  }),
+);
+
+export const IngredientsResult = ({results}: { results: Array<IngredientInfo> }) => {
+  const classes = useStyles();
+
+  if (results.length === 0) {
+    return <h2>No data about any of the ingredients</h2>;
+  }
+
   return (
     <>
-      <h2>{results.length > 0 ? 'Your results' : 'No data about any of the ingredients'}</h2>
-      {results.map((info: IngredientInfo) => <div key={info.uuid}>
-        <b>Name:</b> {info.name}
-        <b>Status:</b> {info.status}
-        <b>Description:</b> {info.description}
-      </div>)}
+      <List className={classes.root}>
+        {results.map((info: IngredientInfo) =>
+          <ListItem key={info.uuid}>
+            <ListItemText primary={info.name} secondary={info.description} />
+          </ListItem>
+        )}
+      </List>
     </>
   );
-}
+};
