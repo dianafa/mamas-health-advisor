aa4f05ef116c5aa15cb59a93637acfd88fb0f357
Diana Marzjan <dianafa@o2.pl>
2021-03-03 19:57:35 +0100

    Add colors to list elements

--- src/components/IngredientsResult.tsx
+++ src/components/IngredientsResult.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {IngredientInfo} from '../types/IngredientInfo';
+import {IngredientInfo, Status} from '../types/IngredientInfo';
 
 import {createStyles, Theme, makeStyles} from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
@@ -13,6 +13,23 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: 'auto',
       backgroundColor: theme.palette.background.paper,
     },
+    // https://www.schemecolor.com/very-pale-rainbow.php
+    // https://www.schemecolor.com/pastel-retro.php
+    [Status.GREEN.toString()]: {
+      background: '#C7E3D0'
+    },
+    [Status.YELLOW.toString()]: {
+      background: '#E7E6CE'
+    },
+    [Status.RED.toString()]: {
+      background: '#E9CCCE'
+    },
+    [Status.UNRESEARCHED.toString()]: {
+      background: '#C9C1E7'
+    },
+    [Status.INVALID.toString()]: {
+      background: theme.palette.background.default
+    }
   }),
 );
 
@@ -27,7 +44,7 @@ export const IngredientsResult = ({results}: { results: Array<IngredientInfo> })
     <>
       <List className={classes.root}>
         {results.map((info: IngredientInfo) =>
-          <ListItem key={info.uuid}>
+          <ListItem key={info.uuid} className={classes[info.status.toString()]}>
             <ListItemText primary={info.name} secondary={info.description} />
           </ListItem>
         )}
