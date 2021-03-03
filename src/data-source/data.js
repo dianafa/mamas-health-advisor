cfa5a4e156fe350c1ff1b9fa9ed3c8844ee788cc
Diana Marzjan <dianafa@o2.pl>
2021-03-03 20:13:35 +0100

    Update data source

--- src/data-source/data.js
+++ src/data-source/data.js
@@ -1,34 +1,67 @@
-const data = {
+export default {
   "lookup": {
+    "alpha-isomethyl lonone": "d18fb880-7472-5f51-b5ff-09a71bc0c498",
     "aqua": "b360de15-f759-5658-9cb2-9cc2ebcf150e",
     "water": "b360de15-f759-5658-9cb2-9cc2ebcf150e",
+    "bacopa monniera extract": "e4681bdf-2f4f-5464-83ab-3328f025b202",
     "beta-glucan": "4c9048d3-588b-5cb6-b4e9-b92d6838e0a4",
+    "bis-ethylhexyloxyphenol methoxyphenyl triazine": "09297be4-6767-562c-ab31-81ae0c6a45f6",
+    "butyl methoxydibenzoylmethane": "e930b52d-9f57-5b33-b68c-a28f9591a3c8",
+    "butylene glycol": "4f80f58a-06a5-5a1f-9dc6-791656fd8153",
+    "c12-15 alkyl benzoate": "04be2235-7d89-5e1b-b798-bffbdc49b857",
+    "cetyl alcohol": "360d5c55-8e40-580c-b869-34caec766fda",
+    "cetyl palmitate": "d95e8a33-a499-5469-beff-f8c4050676c6",
+    "ci 77491 (iron oxides)": "14284165-557e-5c64-94d5-5ba891a4eb74",
     "ci 77891": "a3f6c359-3b15-57d0-8845-76515f90bc42",
     "titanium dioxide": "a3f6c359-3b15-57d0-8845-76515f90bc42",
     "citronellol": "14708a85-ea03-5294-a07b-a69393524eda",
+    "coumarin": "2aa5c03d-b48f-5d0f-b3d9-7b5c9c966a24",
     "diazolidinyl urea": "27ce6c57-758d-5af2-8209-0e32daafd152",
     "dimethicone": "e862186f-7add-5c79-8660-5c74c74ea540",
     "dimethicone/vinyl dimethicone crosspolymer": "2cf45863-19d2-5adb-bed3-f419c8dac29d",
     "dipeptide-2": "9decda69-2332-56fa-b619-37fe07958ead",
+    "disodium edta": "03a96171-991f-5aa3-9d6e-b15677bd1eaa",
+    "ethylhexyl methoxycinnamate": "517b2364-8dd9-560c-9f0c-863765d9d127",
+    "ethylhexylglycerin": "9fe381c1-9088-52d3-9036-b7a1b94dee6c",
     "euterpe oleracea fruit extract": "ddd5fab3-f61f-56c1-836d-ed6a9fc41805",
     "glycerin": "9470e13d-530f-567f-a513-60409d3254e6",
+    "glyceryl stearate": "ec8c9394-ed35-5b35-a56e-d1d573e0497a",
+    "helichrysum stoechas flower extract": "5f6b53a8-5c5d-56dc-9931-8a1d7686d0e6",
     "hesperidin methyl chalcone": "fa49d9ea-ca27-5385-b42a-d5a3b7d4ad4e",
+    "hydrogenated coco-glycerides": "f6edd58c-4c35-5369-a1e2-eeda1aaa1f78",
+    "hydroxycitronellal": "2a21edb8-bac2-590e-ba1a-0aa209341b8a",
+    "hydroxyethylcellulose": "ac627524-2784-57ab-8abe-c76701ce9f1d",
     "limonene": "21ab4761-b4b9-5edd-b448-99127e3d61d0",
     "linalool": "82d986ee-f37e-5e33-bae0-cffadd822932",
+    "marrubium vulgare extract": "17573c45-9086-5976-9c52-48aafabba47c",
     "methylparaben": "318731c7-29ee-5eb0-9a03-b17ebd408c1a",
     "mica": "ee527165-56e6-5082-b4d8-ad86f589886b",
+    "octocrylene": "9a89c2ef-35eb-5550-b7dd-38a07994a0ff",
     "palmitoyl tetrapeptide-7": "3ccc75a0-8df2-5f08-8740-ea6f02c22014",
     "panthenol": "b2b15b41-0d8e-554b-af9e-761adcfc5a81",
     "parfum": "9d8c519d-a834-5823-a1bf-90e696e61c89",
     "fragrance": "9d8c519d-a834-5823-a1bf-90e696e61c89",
+    "peg-100 stearate": "953780c4-5f52-5945-bb9a-d36d80c1cede",
+    "phenoxyethanol": "ce0b810a-5e2b-5352-a766-e3eb54eb6ec7",
+    "polymethyl methacrylate": "d7ec99db-0b57-5458-9894-5add8e09acd7",
+    "potassium cetyl phosphate": "1d721087-e7bd-5e18-95b4-7ae7e3895c10",
     "propylene glycol": "2fed8166-352e-580b-857c-1b543d399d66",
     "propylparaben": "75d35a91-2cb0-56ea-9ef6-5f7970091881",
+    "saccharomyces/copper ferment": "b995ef37-bf72-5ccb-8f7c-34826f12b9d6",
+    "saccharomyces/iron ferment": "63d70c46-4390-565b-ad02-ff2b252951d7",
+    "saccharomyces/magnesium ferment": "d753ebc0-8504-55d2-8046-9def8807ea6c",
+    "saccharomyces/silicon ferment": "85414585-e114-510d-8872-f5c434204a20",
+    "saccharomyces/zinc ferment": "9001cb33-ace4-5153-855e-9ec891158250",
     "sodium hyaluronate": "104293dd-553d-593a-869b-1128131525cf",
     "hyaluronic acid": "104293dd-553d-593a-869b-1128131525cf",
+    "sodium hydroxide": "b4249cc8-93b3-5f21-b071-55c8519887d3",
     "sodium polyacrylate": "8cdcf7a7-e928-539d-ad6f-3a044fcf0a35",
+    "soluble collagen": "862630d3-dc85-51b7-acc3-aa92b5e6da8e",
     "steareth-20": "8e396217-755e-5827-b2ad-bf4d6edee0b7",
+    "synthetic fluorphlogopite": "0dc66fba-a60f-56ef-a66d-70dec98b05f5",
     "tin oxide": "526c6417-4729-5c4d-ab8b-b4afdeec506f",
     "tocopheryl acetate": "8a46d05d-04ca-543e-b905-b0535c15b5b4",
+    "triethylhexanoin": "3091be97-166a-51b3-a3f6-be6827aa7dc1",
     "xanthan gum": "41b09068-ebc1-55ce-831f-302ee508a866",
     "e415": "41b09068-ebc1-55ce-831f-302ee508a866"
   },
@@ -66,36 +99,36 @@ const data = {
       "description": "Limited data suggests no known risks."
     },
     "ddd5fab3-f61f-56c1-836d-ed6a9fc41805": {
-      "status": -1,
-      "description": ""
+      "status": 3,
+      "description": "Limited data suggests no known risks."
     },
     "9470e13d-530f-567f-a513-60409d3254e6": {
-      "status": -1,
-      "description": ""
+      "status": 0,
+      "description": "Look for moisturizing products with ingredients such as glycerin, hyaluronic acid or shea butter, says Doris Day, M.D., clinical assistant professor of dermatology at New York University Medical Center"
     },
     "fa49d9ea-ca27-5385-b42a-d5a3b7d4ad4e": {
-      "status": -1,
-      "description": ""
+      "status": 3,
+      "description": "Limited data suggests no known risk"
     },
     "21ab4761-b4b9-5edd-b448-99127e3d61d0": {
-      "status": -1,
-      "description": ""
+      "status": 1,
+      "description": "It is possible that limonene-containing products increase the pregnant  uterus contractility and their use should  be avoided during pregnancy."
     },
     "82d986ee-f37e-5e33-bae0-cffadd822932": {
       "status": 2,
       "description": "Not recommended during pregnancy. Fragrances are usually made up of other harmful chemicals, like parabens, benzene derivatives, aldehydes. These chemicals are linked to cancer and nervous system issues. They can cause irritation and redness in the applied area."
     },
     "318731c7-29ee-5eb0-9a03-b17ebd408c1a": {
-      "status": -1,
-      "description": ""
+      "status": 1,
+      "description": "Parabens are commonly used as preservatives in cosmetic products – especially in creams and body lotions in order to protect against microbial growth. \"However, the new study suggests parebens including methylparaben, propylparaben, butylparaben may be linked to negative effects if used during pregnancy.\" said UFZ researcher Dr Tobias Polte, senior author of the paper. If you want to play it extra safe, try to avoid products with parabens and phthalates for at least the first trimester, when your baby is developing most rapidly. Look for the word “paraben” (methylparaben, propylparaben, butylparaben and ethylparaben are the most common types) on product labels."
     },
     "ee527165-56e6-5082-b4d8-ad86f589886b": {
-      "status": -1,
-      "description": ""
+      "status": 0,
+      "description": "No known risks."
     },
     "3ccc75a0-8df2-5f08-8740-ea6f02c22014": {
-      "status": -1,
-      "description": ""
+      "status": 3,
+      "description": "Limited information available.\n"
     },
     "b2b15b41-0d8e-554b-af9e-761adcfc5a81": {
       "status": 0,
@@ -139,5 +172,3 @@ const data = {
     }
   }
 }
-
-export default data;
